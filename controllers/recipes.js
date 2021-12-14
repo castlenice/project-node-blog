270d190a7bf1b646b801d4138d5d2065d0f9814c
castlenice <sonja.schoenburg@gmail.com>
2021-12-14 18:30:33 +0100

    changing table name

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -2,7 +2,7 @@ import pool from "../db/pg.js";
 
 export const getRecipes = (req, res) => {
     pool.query("SELECT * FROM recipes")
-        .then((data) => res.json({ authors: data.rows }))
+        .then((data) => res.json({ recipes: data.rows }))
         .catch((err) => console.log(err));
   
 };
